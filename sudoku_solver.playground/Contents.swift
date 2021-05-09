cf0792e404649ff40b6693da8f16758cd18920bc
Gokulraj Kumarasasmy <gkumarassamy@gmail.com>
2021-05-09 16:15:40 -0400

    Version2.0

--- sudoku_solver.playground/Contents.swift
+++ sudoku_solver.playground/Contents.swift
@@ -1,3 +1,29 @@
+func makeBoxes (board: [[Int]]) -> [[Int]] {
+    var boxes = [[Int]]()
+    var box1 = [Int]()
+    var box2 = [Int]()
+    var box3 = [Int]()
+    var index = 0
+    for row in board {
+        if index % 3 == 0 && index != 0{
+            boxes.append(box1);boxes.append(box2);boxes.append(box3)
+            box1 = [Int]()
+            box2 = [Int]()
+            box3 = [Int]()
+        }
+        box1 += row[0...2]
+        box2 += row[3...5]
+        box3 += row[6...8]
+        index += 1
+        if index == 9 {
+            boxes.append(box1);boxes.append(box2);boxes.append(box3)
+        }
+        
+    }
+    return boxes
+}
+
+
 func satisfyBoard (board: [[Int]], num : Int, rowIndex : Int, colIndex : Int) -> Bool {
     if board[rowIndex].contains(num) {
         return false
@@ -6,6 +32,44 @@ func satisfyBoard (board: [[Int]], num : Int, rowIndex : Int, colIndex : Int) ->
         if row[colIndex] == num {
             return false
         }
+    }
+    var boxNum = 0
+    if 0 <= rowIndex && rowIndex <= 2 {
+        if 0 <= colIndex && colIndex <= 2 {
+            boxNum = 0
+        }
+        else if 3 <= colIndex && colIndex <= 5 {
+            boxNum = 1
+        }
+        else if 6 <= colIndex && colIndex <= 8 {
+            boxNum = 2
+        }
+    }
+    else if 3 <= rowIndex && rowIndex <= 5 {
+        if 0 <= colIndex && colIndex <= 2 {
+            boxNum = 3
+        }
+        else if 3 <= colIndex && colIndex <= 5 {
+            boxNum = 4
+        }
+        else if 6 <= colIndex && colIndex <= 8 {
+            boxNum = 5
+        }
+    }
+    else if 6 <= rowIndex && rowIndex <= 8 {
+        if 0 <= colIndex && colIndex <= 2 {
+            boxNum = 6
+        }
+        else if 3 <= colIndex && colIndex <= 5 {
+            boxNum = 7
+        }
+        else if 6 <= colIndex && colIndex <= 8 {
+            boxNum = 8
+        }
+    }
+    let boxes = makeBoxes(board: board)
+    if boxes[boxNum].contains(num) {
+        return false
     }
     return true
 }
@@ -57,19 +121,22 @@ func solveBoard (board: [[Int]]) -> ([[Int]], Bool){
 }
 
 var board = [ // Edit this list
-    [1, 0, 0, 4, 8, 9, 0, 0, 6],
-    [7, 3, 0, 0, 0, 0, 0, 4, 0],
-    [0, 0, 0, 0, 0, 1, 2, 9, 5],
-    [0, 0, 7, 1, 2, 0, 6, 0, 0],
-    [5, 0, 0, 7, 0, 3, 0, 0, 8],
-    [0, 0, 6, 0, 9, 5, 7, 0, 0],
-    [9, 1, 4, 6, 0, 0, 0, 0, 0],
-    [0, 2, 0, 0, 0, 0, 0, 3, 7],
-    [8, 0, 0, 5, 1, 2, 0, 0, 4],
+    [9, 0, 0, 0, 0, 6, 7, 0, 0],
+    [6, 7, 0, 5, 0, 0, 0, 0, 4],
+    [0, 0, 1, 0, 0, 0, 0, 8, 0],
+    [2, 0, 3, 4, 0, 9, 0, 6, 0],
+    [0, 0, 7, 0, 0, 0, 0, 0, 0],
+    [0, 0, 5, 0, 0, 0, 1, 7, 0],
+    [0, 0, 0, 1, 0, 0, 4, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 2, 7],
+    [7, 2, 0, 0, 3, 0, 8, 0, 5],
 ]
 
 var answer = solveBoard(board: board)
 var newAnswer = answer.0
+if !boardFull(board: newAnswer) {
+    print("No Solution")
+}
 for row in newAnswer {
     print(row)
 }
